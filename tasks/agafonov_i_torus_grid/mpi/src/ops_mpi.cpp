07d231ac5adcd42732900c26413e6b8fef243774
ilya-agafonov <ilya-agaf@mail.ru>
2026-01-03 18:24:27 +0000

    fix2

--- tasks/agafonov_i_torus_grid/mpi/src/ops_mpi.cpp
+++ tasks/agafonov_i_torus_grid/mpi/src/ops_mpi.cpp
@@ -2,6 +2,7 @@
 
 #include <mpi.h>
 
+#include <array>
 #include <cmath>
 
 #include "agafonov_i_torus_grid/common/include/common.hpp"
@@ -16,39 +17,61 @@ TorusGridTaskMPI::TorusGridTaskMPI(const InType &in) {
 bool TorusGridTaskMPI::ValidationImpl() {
   int world_size = 0;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
   auto data = GetInput();
 
-  return (data.source_rank >= 0 && data.source_rank < world_size && data.dest_rank >= 0 && data.dest_rank < world_size);
+  return (world_size >= 1 && data.source_rank >= 0 && data.source_rank < world_size && data.dest_rank >= 0 &&
+          data.dest_rank < world_size);
 }
+
 bool TorusGridTaskMPI::PreProcessingImpl() {
   GetOutput() = 0;
   return true;
 }
 
 bool TorusGridTaskMPI::RunImpl() {
+  int world_size = 0;
   int world_rank = 0;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
   auto data = GetInput();
   int res = 0;
+
+  std::array<int, 2> dims = {0, 0};
+  MPI_Dims_create(world_size, 2, dims.data());
+  std::array<int, 2> periods = {1, 1};
+  MPI_Comm torus_comm = MPI_COMM_NULL;
+  MPI_Cart_create(MPI_COMM_WORLD, 2, dims.data(), periods.data(), 0, &torus_comm);
 
   if (data.source_rank == data.dest_rank) {
     if (world_rank == data.source_rank) {
       res = data.value;
     }
   } else {
-    if (world_rank == data.source_rank) {
-      MPI_Send(&data.value, 1, MPI_INT, data.dest_rank, 0, MPI_COMM_WORLD);
-    } else if (world_rank == data.dest_rank) {
-      MPI_Recv(&res, 1, MPI_INT, data.source_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::array<int, 2> src_coords = {0, 0};
+    std::array<int, 2> dest_coords = {0, 0};
+    MPI_Cart_coords(torus_comm, data.source_rank, 2, src_coords.data());
+    MPI_Cart_coords(torus_comm, data.dest_rank, 2, dest_coords.data());
+
+    int src_cart_rank = 0;
+    int dest_cart_rank = 0;
+    MPI_Cart_rank(torus_comm, src_coords.data(), &src_cart_rank);
+    MPI_Cart_rank(torus_comm, dest_coords.data(), &dest_cart_rank);
+
+    if (world_rank == src_cart_rank) {
+      res = data.value;
+      MPI_Send(&res, 1, MPI_INT, dest_cart_rank, 0, torus_comm);
+    } else if (world_rank == dest_cart_rank) {
+      MPI_Recv(&res, 1, MPI_INT, src_cart_rank, 0, torus_comm, MPI_STATUS_IGNORE);
     }
   }
 
   MPI_Bcast(&res, 1, MPI_INT, data.dest_rank, MPI_COMM_WORLD);
-
   GetOutput() = res;
 
+  if (torus_comm != MPI_COMM_NULL) {
+    MPI_Comm_free(&torus_comm);
+  }
   return true;
 }
 
--- tasks/agafonov_i_torus_grid/tests/performance/main.cpp
+++ tasks/agafonov_i_torus_grid/tests/performance/main.cpp
@@ -9,22 +9,31 @@ namespace agafonov_i_torus_grid {
 
 class TorusGridPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  public:
-  TorusGridPerfTests() = default;
+  TorusGridPerfTests() : input_data_({.value = 0, .source_rank = 0, .dest_rank = 0}) {}
 
  protected:
   void SetUp() override {
-    input_data_ = {.value = 12345, .source_rank = 0, .dest_rank = 0};
+    int world_size = 1;
+#ifdef PPC_VERSION_MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+#endif
+
+    input_data_.value = 12345;
+    input_data_.source_rank = 0;
+    input_data_.dest_rank = (world_size > 1) ? (world_size - 1) : 0;
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
     return output_data == 12345;
   }
+
   InType GetTestInputData() final {
-    return input_data_;
+    InType copy = input_data_;
+    return copy;
   }
 
  private:
-  InType input_data_{.value = 0, .source_rank = 0, .dest_rank = 0};
+  InType input_data_;
 };
 
 TEST_P(TorusGridPerfTests, RunPerfModes) {
@@ -37,6 +46,7 @@ const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, TorusGridTaskMPI, TorusGridTaskSEQ>(PPC_SETTINGS_agafonov_i_torus_grid);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
 const auto kPerfTestName = TorusGridPerfTests::CustomPerfTestName;
 
 INSTANTIATE_TEST_SUITE_P(TorusGridPerfTests, TorusGridPerfTests, kGtestValues, kPerfTestName);
